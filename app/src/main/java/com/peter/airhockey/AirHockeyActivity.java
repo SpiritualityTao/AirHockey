9854979380a38117b1228655419d5bf6c3ba1036
PeterPeng <pengtao@kylinos.cn>
2020-08-15 14:48:55 +0800

    1. 引入三角形扇
    2. 添加新的颜色属性渲染

--- app/src/main/java/com/peter/airhockey/AirHockeyActivity.java
+++ app/src/main/java/com/peter/airhockey/AirHockeyActivity.java
@@ -26,6 +26,7 @@ import static android.opengl.GLES20.GL_FLOAT;
 import static android.opengl.GLES20.GL_LINES;
 import static android.opengl.GLES20.GL_POINTS;
 import static android.opengl.GLES20.GL_TRIANGLES;
+import static android.opengl.GLES20.GL_TRIANGLE_FAN;
 import static android.opengl.GLES20.glClear;
 import static android.opengl.GLES20.glClearColor;
 import static android.opengl.GLES20.glDrawArrays;
@@ -92,34 +93,42 @@ public class AirHockeyActivity extends AppCompatActivity {
 
         private static final int POSITION_COMPONMENT_COUNT = 2;
 
-        private static final String U_COLOR = "u_Color";
+        private static final int BYTES_PER_FLOAT = 4;
+
+        private final FloatBuffer vertexData;
+
+        private static final String A_COLOR = "a_Color";
+
+        private static final int COLOR_COMPONMENT_COUNT = 3;
+
+        private static final int STRIDE = (POSITION_COMPONMENT_COUNT + COLOR_COMPONMENT_COUNT) * BYTES_PER_FLOAT;
+
+//        private static final String U_COLOR = "u_Color";
 
         private static final String A_POSITION = "a_Position";
+
+        private int aColorLocation;
 
         private int uColorLocation;
 
         private int aPositionLocation;
 
         float[] tableVerticesWithTriangles = {
-                //Triangle 1
-                -0.5f, -0.5f,
-                0.5f, 0.5f,
-                -0.5f, 0.5f,
-                //Triangle
-                -0.5f, -0.5f,
-                0.5f, -0.5f,
-                0.5f, 0.5f,
+                //Triangle FAN
+                0f, 0f, 1f, 1f, 1f,
+                -0.5f, -0.5f, 0.7f, 0.7f, 0.7f,
+                0.5f, -0.5f, 0.7f, 0.7f, 0.7f,
+                0.5f, 0.5f, 0.7f, 0.7f, 0.7f,
+                -0.5f, 0.5f, 0.7f, 0.7f, 0.7f,
+                -0.5f, -0.5f, 0.7f, 0.7f, 0.7f,
+
                 //Line 1
-                -0.5f, 0f,
-                0.5f, 0f,
+                -0.5f, 0f, 1f, 0f, 0f,
+                0.5f, 0f, 1f, 0f, 0f,
                 //Mallets
-                0f, -0.25f,
-                0f, 0.25f
+                0f, -0.25f, 0f, 0f, 1f,
+                0f, 0.25f, 1f, 0f, 0f,
         };
-
-        private static final int BYTES_PER_FLOAT = 4;
-
-        private final FloatBuffer vertexData;
 
         private int program;
 
@@ -153,16 +162,26 @@ public class AirHockeyActivity extends AppCompatActivity {
             }
             //5.使用程序
             glUseProgram(program);
-            //6.获取uniform和attribute的位置
-            uColorLocation = glGetUniformLocation(program, U_COLOR);
+            //6.获取attribute color和position的位置
+            aColorLocation = glGetAttribLocation(program, A_COLOR);
 
             aPositionLocation = glGetAttribLocation(program, A_POSITION);
+
             //每个缓冲区都有它内部的指针，设置起始位置
             vertexData.position(0);
+
             //6.告诉OpenGL，它可以在缓冲区找到对应a_Position位置
-            glVertexAttribPointer(aPositionLocation, POSITION_COMPONMENT_COUNT, GL_FLOAT, false, 0, vertexData);
-            //7.使能顶点数组
+            glVertexAttribPointer(aPositionLocation, POSITION_COMPONMENT_COUNT, GL_FLOAT, false, STRIDE, vertexData);
+
+            //7.使能顶点
             glEnableVertexAttribArray(aPositionLocation);
+
+            vertexData.position(POSITION_COMPONMENT_COUNT);
+
+            glVertexAttribPointer(aColorLocation, COLOR_COMPONMENT_COUNT, GL_FLOAT, false, STRIDE, vertexData);
+
+            //使能颜色
+            glEnableVertexAttribArray(aColorLocation);
         }
 
         @Override
@@ -176,17 +195,13 @@ public class AirHockeyActivity extends AppCompatActivity {
             glClear(GL_COLOR_BUFFER_BIT);
 
             //前6个点绘制三角形
-            glUniform4f(uColorLocation, 1.0f, 1.0f, 1.0f, 1.0f);
-            glDrawArrays(GL_TRIANGLES, 0, 6);
+            glDrawArrays(GL_TRIANGLE_FAN,0,6);
 
             //接着2个点绘制直线
-            glUniform4f(uColorLocation, 1.0f, 0.0f, 0.0f, 1.0f);
             glDrawArrays(GL_LINES, 6, 2);
 
             //绘制2个颜色不同的点
-            glUniform4f(uColorLocation, 0.0f, 0.0f, 1.0f, 1.0f);
             glDrawArrays(GL_POINTS, 8, 1);
-            glUniform4f(uColorLocation, 1.0f, 0.0f, 0.0f, 1.0f);
             glDrawArrays(GL_POINTS, 9, 1);
         }
     }
